625b77a9f8a475adc9af02080d4ba87ee8efb9cf
Kane Armstrong <kane.armstrong@outlook.com>
2021-02-26 21:18:54 +1300

    AAA comments are a bit meh

--- bypass-auth-in-integration-tests/IntegrationTests/Controllers/BooksController/GetSpec/Getting_books.cs
+++ bypass-auth-in-integration-tests/IntegrationTests/Controllers/BooksController/GetSpec/Getting_books.cs
@@ -11,16 +11,12 @@ namespace IntegrationTests.Controllers.BooksController.GetSpec
         [Fact]
         public async Task returns_200_ok()
         {
-            //+ Arrange
             const string url = "books";
-
             using var factory = new MyWebApplicationFactory();
             var client = factory.CreateClient();
 
-            //+ Act
             using var response = await client.GetAsync(url);
 
-            //+ Assert
             response.StatusCode.Should().Be(StatusCodes.Status200OK);
         }
     }
