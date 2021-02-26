38f10286548ccbbaf1fece5b7233cf4196d95a0a
Kane Armstrong <kane.armstrong@outlook.com>
2021-02-26 14:44:56 +1300

    remove dead code

--- bypass-auth-in-integration-tests/IntegrationTests/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using IntegrationTests.Infrastructure;
-using IntegrationTests.Infrastructure.XUnit;
-using Xunit;
-
-[assembly: TestFramework("IntegrationTests.Infrastructure.XUnit.XunitTestFrameworkWithAssemblyFixture", "IntegrationTests")]
-[assembly: AssemblyFixture(typeof(TestFixture))]
